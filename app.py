feec6c8228a3e9e016ab55dbddde258a19a44add
MRSE435 <m54655967@gmail.com>
2026-07-11 23:20:23 +0530

    CREATED API IN PYTHON FLASK TO SEND inititial data rtequired bu combobox

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import numpy as np
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 pipeline = joblib.load('xgboostmodel_tuned.pkl')
 @app.route('/')
 def hello_world():  # put application's code here
@@ -31,17 +31,33 @@ def chart_data():
     return jsonify(data)
 
 
-@app.route("/data-car_name")
+@app.route("/data-car_names")
 def car_data():
     rawdf = pd.read_csv("car.csv")
-    print(rawdf["car_name"].nunique())
-    print(rawdf.shape)
-    print(len(rawdf))
-    data = rawdf[['car_name']].drop_duplicates().to_dict(orient="records")
+    # print(rawdf["fuel_type"].unique())
+    # print(rawdf["seller_type"].unique())
+    # print(rawdf.shape)
+    # print(len(rawdf))
+    unique_cars = rawdf['car_name'].dropna().unique()
+    # data = rawdf[['car_name']].drop_duplicates().to_dict(orient="records")
+    data = [{"value": car, "label": car} for car in unique_cars]
+
+    return jsonify(data)
+
+@app.route("/data-brand_names")
+def brand_data():
+    rawdf = pd.read_csv("car.csv")
+    data = rawdf[['brand']].drop_duplicates().to_dict(orient="records")
 
     return jsonify(data)
 
 
+@app.route("/data-model_names")
+def model_data():
+    rawdf = pd.read_csv("car.csv")
+    data = rawdf[['model']].drop_duplicates().to_dict(orient="records")
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
